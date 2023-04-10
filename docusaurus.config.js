3dce5f38606dbffca2b17187a987b654df518e0f
Jessica Lin <jlin2700@gmail.com>
2023-04-10 16:55:54 -0700

    Update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://0x.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-   baseUrl: '/docs',
+   baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
