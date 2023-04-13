a8908c654ff559e4ff9a9f1ae9c85264594a687b
Long Nguyen <tobernguyen@gmail.com>
2023-04-13 14:31:11 -0600

    404 page redirects to Home after 3 seconds

---
+++ src/theme/NotFound.js
@@ -0,0 +1,68 @@
+import Translate, { translate } from '@docusaurus/Translate';
+import { PageMetadata } from '@docusaurus/theme-common';
+import Layout from '@theme/Layout';
+import React, { useEffect, useState } from 'react';
+
+const REDIRECT_TO_HOME_AFTER_SEC = 3
+
+export default function NotFound() {
+  const [sec, setSec] = useState(REDIRECT_TO_HOME_AFTER_SEC)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      window.location.replace('/docs')
+    }, REDIRECT_TO_HOME_AFTER_SEC * 1000)
+    const interval = setInterval(() => {
+      setSec(sec => sec - 1)
+    }, 1000)
+    return () => {
+      clearTimeout(timeout)
+      clearInterval(interval)
+    }
+  }, [])
+
+  return (
+    <>
+      <PageMetadata
+        title={translate({
+          id: 'theme.NotFound.title',
+          message: 'Page Not Found',
+        })}
+      />
+      <Layout>
+        <main className="container margin-vert--xl">
+          <div className="row">
+            <div className="col col--6 col--offset-3">
+              <h1 className="hero__title">
+                <Translate
+                  id="theme.NotFound.title"
+                  description="The title of the 404 page">
+                  Page Not Found
+                </Translate>
+              </h1>
+              <p>
+                <Translate
+                  id="theme.NotFound.p1"
+                  description="The first paragraph of the 404 page">
+                  We could not find what you were looking for.
+                </Translate>
+              </p>
+              <p>
+                <Translate
+                  id="theme.NotFound.p2"
+                  description="The 2nd paragraph of the 404 page">
+                  Please contact the owner of the site that linked you to the
+                  original URL and let them know their link is broken.
+                </Translate>
+              </p>
+              <p>
+                <br></br>
+                Will redirect to home in {sec} seconds...
+              </p>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </>
+  );
+}
